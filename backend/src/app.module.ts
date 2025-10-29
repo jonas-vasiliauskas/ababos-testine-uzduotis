1dd055ece10fab316b1a517917a44bb2b2d9ee7a
Jonas Vasiliauskas <vas.jonas@gmail.com>
2025-10-29 22:52:47 +0200

    postgres connected

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+/*import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,4 +7,24 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
+export class AppModule {}*/
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'script',
+      password: 'newpassword',
+      database: 'movie_list_db',
+      autoLoadEntities: true,
+      synchronize: true, // ❗for dev only
+    }),
+  ],
+})
 export class AppModule {}
+

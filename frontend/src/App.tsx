9716e4bf59c10ccb02ee383a141d0bf7208b6086
Jonas Vasiliauskas <vas.jonas@gmail.com>
2025-11-07 17:00:30 +0200

    Wind CSS import fixed (2)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function userLogin(){
+}
+
+export default function App() {
+  
+  return (
+      <div class="grid grid-cols-3 gap-10">
+  <div>
+     col 1 
+  </div>
+  
+  <div>
+     col 2 
+  </div>  
+  
+  <div>
+     col 3 
+  </div>
+</div>
+
+  );
+    
+
+}
+
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,18 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+

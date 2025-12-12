5e84c22b8f37e856664d8c2457b4a8bf7a9efc31
Jonas Vasiliauskas <vas.jonas@gmail.com>
2025-12-12 19:02:51 +0200

    Starting to implement backend using Nest.js

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,10 @@
-/*import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}*/
-
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     
-  ],
+  UsersModule],
 })
 export class AppModule {}
 
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,8 @@
+/*import { Injectable } from '@nestjs/common';*/
+import { Injectable } from '@nestjs/common';
+import { User } from './user.model';
+
+@Injectable()
+export class UsersService {}
+
+
